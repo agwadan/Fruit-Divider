6a88d57a67387ade29f48c22c64d7bb17ee599b5
agwadan <agwa.dan@gmail.com>
2020-08-09 14:46:04 +0300

    Finished foundation for fruit slicing.

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Fruit : MonoBehaviour
 {
 
+    public float startForce = 2f;
+    Rigidbody2D rb;
+   
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
     public GameObject fruitSlicedPrefab;
 
     void OnTriggerEnter2D(Collider2D col){
         if(col.tag == "Blade"){
+            Vector3 direction = (col.transform.position - transform.position).normalized;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+
             Debug.Log("We hit a watermelon");
-            Instantiate(fruitSlicedPrefab, transform.position, transform.rotation);
+            GameObject slicedFruit = Instantiate(fruitSlicedPrefab, transform.position, transform.rotation);
+            Destroy(slicedFruit, 3f);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour{
+    
+    public GameObject fruitPrefab;
+    public Transform[] spawnPoints;
+
+    public float minDelay = .1f,
+                maxDelay = 1f;
+
+    void Start(){
+        StartCoroutine(SpawnFruits());
+    }
+
+    IEnumerator SpawnFruits(){
+        while (true){
+            //Create a delay between spawning fruits.
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(1f);
+            
+            //Random SpawnPoint
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+
+            //Spawn fruit
+            GameObject spawnedFruit = Instantiate(fruitPrefab, spawnPoint.position, spawnPoint.rotation);
+            Destroy(spawnedFruit, 5f);
+        }
+    }
+}
